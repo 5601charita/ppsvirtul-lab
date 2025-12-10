4496af32d5b21ddcd3d93acc2bfa2dce0d07efd1
5601charita <5601charita@gmail.com>
2025-12-10 15:45:44 +0530

    Add files via upload

---
+++ string7.c
@@ -0,0 +1,10 @@
+//String Reverse Function.
+//strrev(char[]);
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char str[]="MOM";
+	strrev(str);
+	printf("\nReversed String is:%s",str);
+}
